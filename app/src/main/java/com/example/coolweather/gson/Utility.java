a19ba800088620adcd980ebe91674b71f8a31a8d
elvin zl <elvin_zl@163.com>
2018-11-21 18:02:22 +0800

    提交选择区域xml，及db包下的省市区bean

---
+++ app/src/main/java/com/example/coolweather/gson/Utility.java
@@ -0,0 +1,92 @@
+package com.example.coolweather.gson;
+
+import android.text.TextUtils;
+
+import com.example.coolweather.db.City;
+import com.example.coolweather.db.County;
+import com.example.coolweather.db.Province;
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utility {
+
+    /**
+     * 解析和处理服务器返回的省级数据
+     * @param response
+     * @return
+     */
+    public static boolean handleProvinceResponse(String response){
+        if(!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i = 0;i < allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析和处理从服务器返回的城市数据
+     * @param response
+     * @param provinceId
+     * @return
+     */
+    public static boolean handleCityResponse(String response,int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            try {
+                JSONArray citys = new JSONArray(response);
+                for(int i = 0;i < citys.length();i++){
+                    JSONObject cityObject = citys.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析和处理服务器返回的县级数据
+     * @param response
+     * @param cityId
+     * @return
+     */
+    public static boolean handleCountResponse(String response,int cityId){
+        if(!TextUtils.isEmpty(response)){
+            try {
+                JSONArray counties = new JSONArray(response);
+                for(int i = 0;i < counties.length();i++){
+                    JSONObject countyObject = counties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getInt("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+}
